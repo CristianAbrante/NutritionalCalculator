4ef18e927b17fb9d137872685267c4e63e85f84a
Cristian Abrante <alu0100945850@ull.edu.es>
2017-10-22 19:16:21 +0100

    Name of food test failed

--- spec/nutritional_calculator_spec.rb
+++ spec/nutritional_calculator_spec.rb
@@ -2,6 +2,14 @@ require "spec_helper"
 
 RSpec.describe NutritionalCalculator::Food do
 
-  
+  before :each do
+    @test_food = NutritionalCalculator::Food.new("Huevo frito")
+  end
+
+  describe "# almacenamiento del nombre" do
+    it "nombre del alimento" do
+      expect(@test_food.name).to eq("Huevo frito")
+    end
+  end
 
 end
