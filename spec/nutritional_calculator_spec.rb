e2b314736170cbb1cff641c9c3ed5734e7bb608f
Cristian Abrante <alu0100945850@ull.edu.es>
2017-10-22 19:00:08 +0100

    Food Class Created

--- spec/nutritional_calculator_spec.rb
+++ spec/nutritional_calculator_spec.rb
@@ -1,11 +1,6 @@
 require "spec_helper"
 
 RSpec.describe NutritionalCalculator do
-  it "has a version number" do
-    expect(NutritionalCalculator::VERSION).not_to be nil
-  end
 
-  it "does something useful" do
-    expect(true).to eq(true)
-  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
 require "nutritional_calculator"
+require 'nutritional_calculator/food'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
