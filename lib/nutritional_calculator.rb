11f9209d8b1476eddcfd4be082fe52a6377d5b8f
Cristian Abrante <alu0100945850@ull.edu.es>
2017-10-24 18:15:04 +0100

    Food configuration readed from file. Also aumented the tests.

--- lib/nutritional_calculator.rb
+++ lib/nutritional_calculator.rb
@@ -1,5 +1,37 @@
-require "nutritional_calculator/version"
+#require "nutritional_calculator/version"
 
 module NutritionalCalculator
-  # Your code goes here...
+
+  def self.read_nutritional_file(file_name)
+
+    nutritional_hash = Hash.new
+
+    File.open(file_name, "r") do |f|
+
+      f.each_line do |line|
+        vector = line.split(" ")
+
+        if vector.length == 4
+          nutritional_hash[vector[0]] = vector[1...4].map {|value| value.to_f}
+        else
+
+          pos = 1
+          food_name = vector[0]
+
+          while vector[pos].to_f == 0.0 && vector[pos] != "#{vector[pos].to_f}"
+            food_name += " " + vector[pos]
+            pos += 1
+          end
+
+          nutritional_hash[food_name] = vector[pos...vector.length].map {|value| value.to_f}
+        end
+
+      end
+
+    end
+
+    return nutritional_hash
+
+  end
+
 end
--- spec/nutritional_calculator_spec.rb
+++ spec/nutritional_calculator_spec.rb
@@ -3,12 +3,25 @@ require "spec_helper"
 RSpec.describe NutritionalCalculator::Food do
 
   before :each do
-    @test_food = NutritionalCalculator::Food.new("Huevo frito", 14.1, 0.0, 19.5)
+
+    nutritional_hash = NutritionalCalculator::read_nutritional_file "lib/files/nutritional_file.txt"
+
+    @food_vector = []
+    pos = 0
+
+    nutritional_hash.each do |food_name, nutrients|
+      @food_vector.push(NutritionalCalculator::Food.new(food_name, nutrients[0], nutrients[1], nutrients[2]))
+    end
+
   end
 
   describe "Almacenamiento del nombre" do
     it "nombre del alimento" do
-      expect(@test_food.name).to eq("Huevo frito")
+      expect(@food_vector[0].name).to eq("Huevo frito")
+      expect(@food_vector[1].name).to eq("Leche vaca")
+      expect(@food_vector[2].name).to eq("Yogurt")
+      expect(@food_vector[3].name).to eq("Cerdo")
+      expect(@food_vector[4].name).to eq("Ternera")
     end
   end
 
@@ -31,28 +44,44 @@ RSpec.describe NutritionalCalculator::Food do
   describe "Almacenamiento y acceso del valor de los macronutrientes." do
 
     it "Valor de las proteínas" do
-      expect(@test_food.proteins).to eq(14.1)
+      expect(@food_vector[0].proteins).to eq(14.1)
+      expect(@food_vector[1].proteins).to eq(3.3)
+      expect(@food_vector[2].proteins).to eq(0.0)
+      expect(@food_vector[3].proteins).to eq(21.5)
+      expect(@food_vector[4].proteins).to eq(21.1)
     end
 
     it "Valor de los glúcidos" do
-      expect(@test_food.carbohydrates).to eq(0.0)
+      expect(@food_vector[0].carbohydrates).to eq(0.0)
+      expect(@food_vector[1].carbohydrates).to eq(4.8)
+      expect(@food_vector[2].carbohydrates).to eq(4.9)
+      expect(@food_vector[3].carbohydrates).to eq(0.0)
+      expect(@food_vector[4].carbohydrates).to eq(0.0)
     end
 
     it "Valor de los lípidos" do
-      expect(@test_food.lipids).to eq(19.5)
+      expect(@food_vector[0].lipids).to eq(19.5)
+      expect(@food_vector[1].lipids).to eq(3.2)
+      expect(@food_vector[2].lipids).to eq(3.8)
+      expect(@food_vector[3].lipids).to eq(6.3)
+      expect(@food_vector[4].lipids).to eq(3.1)
     end
 
   end
 
   describe "Valor del alimento formateado" do
     it "valor formateado" do
-      expect("#{@test_food}").to eq("Huevo frito -> proteínas(14.1) glúcidos(0.0) lípidos(19.5)")
+      expect("#{@food_vector[0]}").to eq("Huevo frito -> proteínas(14.1) glúcidos(0.0) lípidos(19.5)")
     end
   end
 
   describe "Valor nutricional del alimento" do
     it "valor nutricional" do
-      expect(@test_food.get_nutritional_value).to eq(231.9)
+      expect(@food_vector[0].get_nutritional_value).to eq(231.9)
+      expect(@food_vector[1].get_nutritional_value).to eq(61.2)
+      expect(@food_vector[2].get_nutritional_value).to eq(53.8)
+      expect(@food_vector[3].get_nutritional_value).to eq(142.7)
+      expect(@food_vector[4].get_nutritional_value).to eq(112.30000000000001)
     end
   end
 
