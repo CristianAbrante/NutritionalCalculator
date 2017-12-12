872a974f05b94239b3cb39a913e0620df255651c
Cristian Abrante <alu0100945850@ull.edu.es>
2017-12-12 23:51:55 +0000

    food database tests added

--- spec/plate_spec.rb
+++ spec/plate_spec.rb
@@ -10,5 +10,16 @@ RSpec.describe NutritionalCalculator::Plate do
       expect(@test_plate.name).to eq("plato de prueba")
     end
 
+    it "Testeamos la base de datos del plato" do
+      test_database = NutritionalCalculator::get_food_vector("lib/files/food_database.config")
+
+      pos = 0
+      @test_plate.food_database.each do |food_name, food|
+        expect(test_database[pos] == food).to be(true)
+        pos += 1
+      end
+
+    end
+
   end
 end
