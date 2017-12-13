22e9300a99de3dab733ea0684b3b731c16df4783
Cristian Abrante <alu0100945850@ull.edu.es>
2017-12-13 00:28:01 +0000

    Plate parts tests passed

--- lib/nutritional_calculator/plate.rb
+++ lib/nutritional_calculator/plate.rb
@@ -11,13 +11,46 @@ module NutritionalCalculator
     DATABASE_FILE = "lib/files/food_database.config"
 
     # Atributos que podremos leer
-    attr_reader :name, :food_database
+    attr_reader :name, :food_database, :vegetables, :fruits, :cereals, :proteins, :oils, :drinks
 
     # Constructor de la clase
     # @param name [string] nombre que tendrá el plato
     def initialize(name)
       @name = name
       @food_database = read_food_database(DATABASE_FILE)
+
+      @vegetables = {}
+      @fruits = {}
+      @cereals = {}
+      @proteins = {}
+      @oils = {}
+      @drinks = {}
+    end
+
+    # Métodos para añadir elementos a las partes del plato.
+
+    def vegetable(food_name, quantity = {})
+        add_food_to_plate("vegetable", food_name, quantity[:amount]) if(quantity[:amount])
+    end
+
+    def fruit(food_name, quantity = {})
+        add_food_to_plate("fruit", food_name, quantity[:amount]) if(quantity[:amount])
+    end
+
+    def cereal(food_name, quantity = {})
+        add_food_to_plate("cereal", food_name, quantity[:amount]) if(quantity[:amount])
+    end
+
+    def protein(food_name, quantity = {})
+        add_food_to_plate("protein", food_name, quantity[:amount]) if(quantity[:amount])
+    end
+
+    def oil(food_name, quantity = {})
+        add_food_to_plate("oil", food_name, quantity[:amount]) if(quantity[:amount])
+    end
+
+    def drink(food_name, quantity = {})
+        add_food_to_plate("drink", food_name, quantity[:amount]) if(quantity[:amount])
     end
 
     private
@@ -45,6 +78,23 @@ module NutritionalCalculator
       end
 
       food_database
+    end
+
+    def add_food_to_plate(food_kind, food_name, str_amount)
+      case food_kind
+      when "vegetable"
+        @vegetables[food_name] = str_amount
+      when "fruit"
+        @fruits[food_name] = str_amount
+      when "cereal"
+        @cereals[food_name] = str_amount
+      when "protein"
+        @proteins[food_name] = str_amount
+      when "oil"
+        @oils[food_name] = str_amount
+      when "drink"
+        @drinks[food_name] = str_amount
+      end
     end
   end
 end
