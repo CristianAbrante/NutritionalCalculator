b41dbd65b708782a60f4b55e2c0eff1eeb715e31
Cristian Abrante <alu0100945850@ull.edu.es>
2017-12-13 00:16:54 +0000

    Plate elements tests failed

--- spec/plate_spec.rb
+++ spec/plate_spec.rb
@@ -18,8 +18,36 @@ RSpec.describe NutritionalCalculator::Plate do
         expect(test_database[pos] == food).to be(true)
         pos += 1
       end
-
     end
 
+    it "El alimento debe poder almacenar y acceder a los vegetales" do
+      @test_plate.vegetable "Tomate", :amount => "2 piezas"
+      expect(@test_plate.vegetables["Tomate"]).to be("2 piezas")
+    end
+
+    it "El alimento debe poder almacenar y acceder a las frutas" do
+      @test_plate.fruit "Plátano", :amount => "20 gr"
+      expect(@test_plate.fruits["Plátano"]).to be("20 gr")
+    end
+
+    it "El alimento debe poder almacenar y acceder a los cereales" do
+      @test_plate.cereal "Arroz", :amount => "1 taza grande"
+      expect(@test_plate.cereals["Arroz"]).to be("1 taza grande")
+    end
+
+    it "El alimento debe poder almacenar y acceder a las proteínas" do
+      @test_plate.protein "Lentejas", :amount => "1/2 taza"
+      expect(@test_plate.vegetables["Lentejas"]).to be("1/2 taza")
+    end
+
+    it "El alimento debe poder almacenar y acceder a los aceites" do
+      @test_plate.vegetable "Aceite de oliva", :amount => "1 chorrito"
+      expect(@test_plate.vegetables["Aceite de oliva"]).to be("1 chorrito")
+    end
+
+    it "El alimento debe poder almacenar y acceder a las bebidas" do
+      @test_plate.vegetable "agua", :amount => "1 litro"
+      expect(@test_plate.vegetables["agua"]).to be("1 litro")
+    end
   end
 end
